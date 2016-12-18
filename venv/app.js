4e3d3ce37d40dc111699ff923cc9a3e83c5c9f2d
Derek <zhanhangsky@gmail.com>
2016-12-18 12:33:19 -0500

    Rewriting algorithm into JS

--- venv/app.js
+++ venv/app.js
@@ -12,8 +12,6 @@ angular.module('nchill', [])
       $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
       .then(function(response){ $scope.details = response.data; });
 
-      $http.get("http://www.omdbapi.com/?s=" + $scope.search)
-      .then(function(response){ $scope.related = response.data; });
     }
 
     $scope.update = function(movie){
